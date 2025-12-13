7b3c4a79e2fcfc86c75396771902eb7b01a1c1de
ASM@GitHub2024 <asmaa14has@gmail.com>
2025-12-13 09:10:17 +0200

    create Drawer from MaterialUI for sidebar and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import TopBar from "./components/TopBar";
+import SideBar from "./components/SideBar";
 
-function App() {
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+export default function App() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <>
-      Hello ........
-    </>
-  )
-}
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <TopBar open={open} handleDrawerOpen={handleDrawerOpen} />
 
-export default App
+      <SideBar open={open} handleDrawerClose={handleDrawerClose} />
+
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+        <Typography sx={{ marginBottom: 2 }}>
+          content ...............
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
---
+++ src/components/TopBar.jsx
@@ -0,0 +1,63 @@
+import Toolbar from "@mui/material/Toolbar";
+import MenuIcon from "@mui/icons-material/Menu";
+import MuiAppBar from "@mui/material/AppBar";
+import { styled } from "@mui/material/styles";
+import { IconButton, Typography } from "@mui/material";
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  variants: [
+    {
+      // @ts-ignore
+      props: ({ open }) => open,
+      style: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(["width", "margin"], {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+      },
+    },
+  ],
+}));
+
+export default function TopBar({ open, handleDrawerOpen }) {
+  return (
+    <>
+      <AppBar
+        position="fixed"
+        // @ts-ignore
+        open={open}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={[
+              {
+                marginRight: 5,
+              },
+              open && { display: "none" },
+            ]}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Mini variant drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+}
